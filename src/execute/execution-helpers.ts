6805aa70a64791df0faca01a9ddc122191f20dd5
seveibar <seveibar@gmail.com>
2023-03-18 14:00:06 -0700

    wip

--- src/execute/execution-helpers.ts
+++ src/execute/execution-helpers.ts
@@ -1,1 +1,13 @@
-export const getExecutionHelpers = () => {}
+import { Configuration, OpenAIApi } from "openai"
+
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+})
+
+export const getExecutionHelpers = async () => {
+  return {
+    openai: new OpenAIApi(configuration),
+  }
+}
+
+export type ExecutionHelpers = ReturnType<typeof getExecutionHelpers>
---
+++ src/execute/index.ts
@@ -0,0 +1,3 @@
+export * from "./execution-context"
+export * from "./execution-helpers"
+export * as instruction_functions from "./instruction-functions"
